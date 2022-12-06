bf434feb5a5bc350db494bccae770dc5800e0f58
olmytro <ironbro24@gmail.com>
2022-12-06 19:11:25 +0300

    Add file

---
+++ box/Boxer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace box
+{
+    public class Boxer
+    {
+        public string name;
+        static public int playerHp = 10;
+        static public int computerHp = 10;
+        static public bool status = true;
+        static public int roundCount = 3;
+        static public int oneRoundTime = 5;
+
+        public Boxer(string Name)
+        {
+            this.name = Name;
+        }
+
+        private bool ChanceOfPunch()
+        {
+            Random rnd = new Random();
+            int generateChance = rnd.Next(1, 10);
+            if (generateChance % 2 == 0)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+
+        public void Punch()
+        {
+
+            Console.WriteLine($"{name} замахивается");
+            if (ChanceOfPunch() == true)
+            {
+                Console.WriteLine("попал");
+                if (this.name == "Player")
+                {
+                    Bobo("Computer");
+                }
+                else
+                {
+                    Bobo("Player");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("промазал");
+            }  
+        }
+        public static int Bobo(string boxer)
+        {
+            if (boxer == "Computer")
+            {
+                Console.WriteLine("Computer missed the punch");
+                return computerHp--;
+            }
+            else
+            {
+                Console.WriteLine("Player missed the punch");
+                return playerHp--;
+            }
+                
+        }
+        public static void HpZero(string hp)
+        {
+            if(hp == "Player")
+            Console.WriteLine("The Player doesn't have hp. The Player lost");
+            else
+                Console.WriteLine("The Computer doesn't have hp. The Computer lost");
+        }
+        public static void RoundEnd(int round, int playerHp, int computerHp)
+        {
+            Console.WriteLine($"Round {round} end");
+            Console.WriteLine($"Player hp is: {playerHp}");
+            Console.WriteLine($"Computer hp is: {computerHp}");   
+        }
+        public static void HpMore(int playerHp, int computerHp)
+        {
+            Console.WriteLine("Result.");
+            if (playerHp > computerHp)
+                Console.WriteLine("The Player has more hp and win!");
+            else if(playerHp == computerHp)
+                Console.WriteLine("There is Draw!");
+            else
+                Console.WriteLine("The Computer has more hp and win!");
+        }
+    }
+}
---
+++ box/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boxer Player = new Boxer("Player");
+            Boxer Computer = new Boxer("Computer");
+            for (int j = 0; j < Boxer.roundCount; j++)
+            {
+                Console.WriteLine($"Round {j} start");
+                for (int i = 0; i < Boxer.oneRoundTime; i++)
+                {
+                    Console.WriteLine(new string('-', 20));
+                    Player.Punch();
+                    if(Boxer.computerHp == 0)
+                    {
+                        return;
+                    }
+                    Computer.Punch();
+                    if (Boxer.playerHp == 0)
+                        return;
+                }
+                if (Boxer.computerHp == 0 || Boxer.playerHp == 0)
+                {
+                    if (Boxer.playerHp == 0)
+                        Boxer.HpZero("Player");
+                    else
+                        Boxer.HpZero("Computer");
+                }
+                else
+                {
+                    Console.WriteLine(new string('-', 20));
+                    Boxer.RoundEnd(j, Boxer.playerHp, Boxer.computerHp);
+                    Console.WriteLine(new string('-', 20));
+                }
+            }
+            Boxer.HpMore(Boxer.playerHp, Boxer.computerHp);
+        }
+    }
+}
